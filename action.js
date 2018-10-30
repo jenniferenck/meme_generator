e694d4f8d0b9e99fb8dcf8e15800a307c531848e
Jennifer Enck <jenniferenck@mac.com>
2018-10-30 13:50:56 -0700

    edits to javascript structure and css

--- action.js
+++ action.js
@@ -2,17 +2,31 @@ var imageURL = document.getElementById('image-link').value;
 const topInput = document.getElementById('top-input');
 const bottomInput = document.getElementById('bottom-input');
 const addMeme = document.getElementById('add-meme');
+const memeContainer = document.getElementById('meme-container')
 
 var imgArr = [];
 
 function createMeme (index){
-    
+    // create new element structure:
     var meme = document.createElement('div');
     meme.classList.add('meme');
-    //var image = document.createElement('img');
-    //image.src = imageURL;
-    //meme.appendChild('image');
-    document.getElementById('meme-container').appendChild(meme);
+
+    var topCaption = document.createElement('p');
+    topCaption.innerHTML = topInput.value;
+    topCaption.classList.add('top-meme');
+
+    var bottomCaption = document.createElement('p');
+    bottomCaption.innerHTML = bottomInput.value;
+    bottomCaption.classList.add('bottom-meme');
+
+    var image = document.createElement('img');
+    image.src = imageURL;
+    
+    // append children:
+    memeContainer.appendChild(meme);
+    meme.appendChild(image);
+    meme.appendChild(topCaption);
+    meme.appendChild(bottomCaption);
 }
 
 // adding EVENT LISTENERS
